a6491bc7689ff11e1f0eb8cb2987041f81c1496c
allen.chang <allen.chang@ecpay.com.tw>
2025-08-05 15:00:55 +0800

    v2.4.2508041

---
+++ view/adminhtml/web/js/order/gift-options-init.js
@@ -0,0 +1,29 @@
+define([
+    "prototype",
+    "Magento_Sales/order/giftoptions_tooltip"
+], function () {
+    /**
+     * Retrieve gift options tooltip content
+     */
+    function getGiftOptionsTooltipContent(itemId) {
+        var contentLines = [];
+        var headerLine = null;
+        var contentLine = null;
+
+        $$(`#gift_options_data_${itemId} .gift-options-tooltip-content`).each(function (element) {
+            if (element.down(0)) {
+                headerLine = element.down(0).innerHTML;
+                contentLine = element.down(0).next().innerHTML;
+                if (contentLine.length > 30) {
+                    contentLine = contentLine.slice(0, 30) + '...';
+                }
+                contentLines.push(headerLine + ' ' + contentLine);
+            }
+        });
+
+        return contentLines.join('<br/>');
+    }
+
+    giftOptionsTooltip.setTooltipContentLoaderFunction(getGiftOptionsTooltipContent);
+    window.getGiftOptionsTooltipContent = getGiftOptionsTooltipContent;
+});
